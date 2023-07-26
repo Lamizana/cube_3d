51faa22e96bdfcacbb93bacfef78a8fb5b8a59c5
Alex Lamizana <alamizan@1F3.42angouleme.fr>
2023-07-26 15:14:38 +0200

    add param gnl in parsing_texture

--- include/cub3d_proto.h
+++ include/cub3d_proto.h
@@ -7,6 +7,6 @@
 /*          PARSING                */
 
 int	parsing(char *file, t_texture *texture, t_map *map);
-int	parse_texture(char *argv, t_texture *texture);
+int	parse_texture(t_texture *texture, int fd_map);
 
 #endif
