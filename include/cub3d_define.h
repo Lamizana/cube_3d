c1939f3ffef5366d207924a489949c59059c778a
Nil Minotte-Cercus <nminotte@1E4.42angouleme.fr>
2023-07-26 11:25:55 +0200

    add

---
+++ include/cub3d_define.h
@@ -0,0 +1,4 @@
+#ifndef CUB3D_DEFINE_H
+# define CUB3D_DEFINE_H
+
+#endif
---
+++ include/cub3d_proto.h
@@ -0,0 +1,8 @@
+#ifndef CUB3D_PROTO_H
+# define CUB3D_PROTO_H
+
+/*          PARSING                */
+
+int	parsing(char *map);
+
+#endif
